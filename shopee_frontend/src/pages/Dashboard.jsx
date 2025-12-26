e8d7dbd2a59ec4dd5cd4c7e6e9daaf371a6f2546
Sanidhyavyas <sanidhya.svyas@gmail.com>
2025-12-26 23:12:34 +0530

    update

---
+++ shopee_frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import "../styles/auth.css";
+
+function Dashboard() {
+  return (
+    <div className="page">
+      <div className="card">
+        <h2>Dashboard</h2>
+        <p>You are successfully logged in.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ shopee_frontend/src/pages/Login.jsx
@@ -0,0 +1,44 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "../styles/auth.css";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (username === "admin" && password === "123") {
+      navigate("/dashboard");
+    } else {
+      alert("Invalid credentials");
+    }
+  }
+
+  return (
+    <div className="page">
+      <form className="card" onSubmit={handleLogin}>
+        <h2>Login</h2>
+
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
