e6232605308bd91d129447d48e25d69fc0e986f6
Sahil Thakur <thakursahil1912@gmail.com>
2025-12-31 15:30:50 +0530

    backend restructuring

--- shopee-backend/src/test/java/com/shopee/shopee_backend/ShopeeBackendApplicationTests.java
+++ shopee-backend/src/test/java/com/shopee/shopee_backend/ShopeeBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.shopee.shopee_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ShopeeBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
