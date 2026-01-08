4c1d8451a0b82876adc9a2c9af8175221345cd00
Sanidhyavyas <sanidhya.svyas@gmail.com>
2026-01-08 22:51:14 +0530

    Update ShopeeBackendApplication.java

--- shopee-backend/src/main/java/com/shopee/shopee_backend/ShopeeBackendApplication.java
+++ shopee-backend/src/main/java/com/shopee/shopee_backend/ShopeeBackendApplication.java
@@ -2,9 +2,6 @@ package com.shopee.shopee_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShopeeBackendApplication {
@@ -13,17 +10,4 @@ public class ShopeeBackendApplication {
 		SpringApplication.run(ShopeeBackendApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173") // Vite dev server
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
-	}
 }
